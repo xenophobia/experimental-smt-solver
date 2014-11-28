cf874be0aa1a1ffcdc297f6479a9d4a540c6c478
kuwahara <kuwahara111011@gmail.com>
2014-11-28 19:13:04 +0900

    Extend domain of variables to positive/negative integer

--- src/Data/SMT/BitBlasting/Main.hs
+++ src/Data/SMT/BitBlasting/Main.hs
@@ -6,14 +6,21 @@ import Data.SMT.BitBlasting.Solver
 import Data.SMT.Solution
 
 defaultThreshold :: Int
-defaultThreshold = 3
+defaultThreshold = 10
+
+incrementalTrial :: Monad m => Int -> Int -> (Int -> m (Maybe SMTSolution)) -> m (Maybe SMTSolution)
+incrementalTrial i n act | i >= n = act i
+                         | otherwise = do
+                             act i >>= \case
+                               Just answer@(Satisfied _) -> return $ Just answer
+                               _ -> incrementalTrial (i+1) n act
 
 main :: IO ()
 main = do
   input <- getLine
   parsed <- parseSMT input
   putStrLn $ "Constraint: " ++ show parsed
-  bitblasting defaultThreshold parsed >>= \case
+  incrementalTrial 2 defaultThreshold (flip bitblasting parsed) >>= \case
     Nothing -> putStrLn "Failed to solve"
     Just Unsatisfied -> putStrLn $ "Unsatisfiable (in threshold = " ++ show defaultThreshold ++ ")"
     Just (Satisfied ans) -> putStrLn $ "Satisfiable by: " ++ show ans
--- src/Data/SMT/BitBlasting/Solver.hs
+++ src/Data/SMT/BitBlasting/Solver.hs
@@ -1,6 +1,7 @@
+{-# Language ViewPatterns #-}
 module Data.SMT.BitBlasting.Solver where
 
-import Prelude hiding ((&&))
+import Prelude hiding ((&&), (||))
 import Data.Bits
 import Data.List (intersect)
 import Data.IntMap (IntMap, union, singleton)
@@ -17,13 +18,18 @@ instance Equatable a => Equatable (IntMap a) where
   m1 === m2 = foldl (&&) true $ map (\k -> m1 IM.! k === m2 IM.! k) overlap
     where overlap = IM.keys m1 `intersect` IM.keys m2
 
+decodeToTwoComplement :: Int -> [Bool] -> Int
+decodeToTwoComplement _ [] = undefined
+decodeToTwoComplement width (map (\x -> if x then 1 else 0) -> b:bs) = 
+  foldl (\acc b -> acc*2+b) 0 (-b:bs)
+
 bitblasting :: Int -> SMT -> IO (Maybe SMTSolution)
 bitblasting width smt = do
   (result, ~(Just answer)) <- minisat `solveWith` flattened
   case result of
     Unsolved -> return Nothing
     Ersatz.Unsatisfied -> return $ Just (Data.SMT.Solution.Unsatisfied)
-    Ersatz.Satisfied -> return $ Just (Data.SMT.Solution.Satisfied (IM.map (foldl (\acc b -> acc*2+(if b then 1 else 0)) 0) answer))
+    Ersatz.Satisfied -> return $ Just (Data.SMT.Solution.Satisfied (IM.map (decodeToTwoComplement width) answer))
     where flattened = flattening width smt
 
 flattening :: (MonadState s m, HasSAT s) => Int -> SMT -> m (IntMap [Bit])
@@ -40,7 +46,8 @@ flattening width (smt1 :&: smt2) = do
   return $ m1 `union` m2
 
 flatteningTerm :: (MonadState s m, HasSAT s) => Int -> Term -> m ([Bit], IntMap [Bit])
-flatteningTerm width (Const n) = return $ ([if testBit n i then true else false | i <- [width-1, width-2 .. 0]], IM.empty)
+flatteningTerm width (Const n) | -(2^(width-1)) <= n && n < 2^(width-1) = return $ ([if testBit n i then true else false | i <- [width-1, width-2 .. 0]], IM.empty)
+                               | otherwise = assert false >> return (replicate width false, IM.empty)                             
 flatteningTerm width (Var n) = do
   bs <- replicateM width exists
   return $ (bs, singleton n bs)
@@ -48,14 +55,15 @@ flatteningTerm width (t1 :+: t2) = do
   (t1Flattened, m1) <- flatteningTerm width t1
   (t2Flattened, m2) <- flatteningTerm width t2
   flattened <- adder width t1Flattened t2Flattened
+  assert $ (head t1Flattened /== head t2Flattened)
+           || (head t1Flattened === head flattened) -- overflow detection
   return (flattened, m1 `union` m2)
 flatteningTerm _ _ = undefined
 
 adder :: (MonadState s m, HasSAT s) => Int -> [Bit] -> [Bit] -> m [Bit]
 adder width bs1 bs2 = do
   bs <- replicateM width exists
-  overflowBit <- foldM aux false $ (reverse $ zip3 bs1 bs2 bs)
-  assert $ overflowBit === false
+  foldM_ aux false $ (reverse $ zip3 bs1 bs2 bs)
   return bs
   where
     aux cin (b1, b2, b) = do
